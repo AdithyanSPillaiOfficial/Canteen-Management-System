f8667c28fd508228fb981f44ad7e99e3ad71d54f
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2023-11-25 00:41:05 +0530

    Test Commit

--- src/generalInfo.jsx
+++ src/generalInfo.jsx
@@ -1,1 +1,2 @@
-export const serverAddress = 'https://canteen-management-system-server.adithyang4media.repl.co' //|| 'http://localhost:5000' ||  process.env['SERVER_ADDRESS'];
+export const serverAddress = 'https://canteen-management-system-server.adithyang4media.repl.co' 
+//|| 'http://localhost:5000' ||  process.env['SERVER_ADDRESS'];
